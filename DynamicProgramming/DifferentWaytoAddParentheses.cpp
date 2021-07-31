df47102066fb3f9772bb66bb141bb6f0db12ca2d
Ankur Kejriwal <ankurkejriwal@ankurs-mbp.lan>
2021-07-31 17:51:25 -0400

    Working recursion method

---
+++ DynamicProgramming/DifferentWaytoAddParentheses.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+
+
+// 4 Steps
+// 1. Bounds of I and J: I = 0 ,  J = string.length()-1
+// 2. Base condition: 
+// 3. K Loop:
+// 4. Function that transforms Temp ans -> final ans: 
+
+//Break at the operatior
+
+// ( 2 * 3 - 4 )* 5 
+// Breaks into i->k-1 and k+1 -> j
+
+// int solve(string str, int i, int j, vector<int> nums){
+//   int i = 0;
+//   int j = str.length()-1;}
+
+
+vector<int> diffWaysBrackets(string str){
+  vector<int> solution;
+  for(int k = 1; k<str.size()-2; k+=2){
+    if(str[i] == '*' || str[i] == '+' || str[i] == '-'){
+      vector<int> left = diffWaysBrackets(str.substr(0,i));
+      vector<int> right = diffWaysBrackets(str.substr(i+1));
+      for(auto &num1 : left){
+        for(auto &num2 : right){
+          if(str[i] == '*') solution.push_back(num1*num2);
+          if(str[i] == '-') solution.push_back(num1-num2);
+          if(str[i] == '+') solution.push_back(num1+num2);
+        }
+      }
+
+    }
+  }
+  if (solution.empty()) solution.push_back(stoi(str));
+
+    return solution; 
+}
+int main()
+{
+  string expression = "2*3-4*5";
+  vector<int> result = diffWaysBrackets(expression);
+
+  for(auto &num : result){
+    cout<<num<<"";
+  }
+
+}
+
+
