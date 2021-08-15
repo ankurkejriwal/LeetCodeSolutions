b1e16cf3153b4551691e92c1d4c9e1671adb82ae
Ankur Kejriwal <ankurkejriwal@ankurs-mbp.lan>
2021-08-15 12:34:27 -0400

    Added DFS Stack Implementation

--- Graphs/baseGraphs.cpp
+++ Graphs/baseGraphs.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <vector>
 #include <unordered_map>
+#include <stack>
 
 //g++ -std=c++11 .cpp -o      
 
@@ -21,14 +22,41 @@ void buildGraph (vector<vector<int>>& trust,unordered_map<int,vector<int>> &grap
     }
 }
 
+void DFSTraversal(unordered_map<string,vector<string>> graph, string src){
+  stack<string> st;
+  st.push(src);
+
+  while(st.size() > 0){
+    string top = st.top();
+    st.pop();
+    cout<<top<<" ";
+
+    for (auto &neighbor : graph[top]){
+      //cout<<"Neighbor for"<<top<<"Value added"<<neighbor<<endl;
+      st.push(neighbor);
+    }
+
+  }
+}
+
+
+
 int main()
 {
   unordered_map<int,vector<int>> graph;
   vector<vector<int>> nums = {{1,3},{1,4},{2,3},{2,4},{4,3}};
   buildGraph(nums,graph);
 
-  cout<<graph.size()<<endl;
-  
+  unordered_map<string,vector<string>> dfs;
+  dfs["a"].push_back("b");
+  dfs["a"].push_back("c");
+  dfs["b"].push_back("d");
+  dfs["c"].push_back("e");
+  dfs["d"].push_back("f");
+
+
+  DFSTraversal(dfs,"a");
+
 }
 
 
